f25526292b3e72162821526b1016c4107d05648f
Chen Cong <craterone11@gmail.com>
2020-05-14 12:03:47 +0900

    add min

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Dugon Web SDK (Typescript)
 
 [![GitHub tag](https://img.shields.io/github/tag/0-u-0/dugon-web-sdk.svg)](https://github.com/0-u-0/dugon-web-sdk)
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](./LICENSE)
 
 ## Build
 
+**dev**
+
 ```bash
-$ rollup -c
+$ npm run-script dev
+```
+
+**release**
+
+```bash
+$ npm run-script release 
 ```
 
 ## Documentation
--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,43 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';//Convert CommonJS modules to ES6
+import { terser } from "rollup-plugin-terser";
+import pkg from './package.json';
 
-export default {
-  input: 'src/dugon.ts',
-  output: {
+
+//FIXME(CC): remove alpha after sdk becomes stable
+const banner = `/**
+ * ${pkg.description} v${pkg.version}-alpha
+ * @author: ${pkg.author}
+ * @license: ${pkg.license}
+ * @repository: ${pkg.repository.url}
+ * @date: ${new Date().toString()}
+ **/
+`;
+
+
+let output;
+
+if (process.env.NODE_ENV === 'production') {
+  //TODO(CC): add map for debug
+  output = {
+    file: 'dst/dugon.min.js',
+    format: 'umd',
+    name: 'Dugon',
+    plugins: [terser()],
+    banner
+  };
+} else {
+  output = {
     file: 'dst/dugon.js',
     format: 'umd',
     name: 'Dugon',
-  },
-  plugins: [typescript({ tsconfig: './tsconfig.json' }),resolve(),commonjs()]
+  };
+}
+
+
+export default {
+  input: 'src/dugon.ts',
+  output,
+  plugins: [typescript({ tsconfig: './tsconfig.json' }), resolve(), commonjs()]
 };
